1aeeba4637c6a3e0bb8267d664a5b842e9f191ee
OyakXD <kayovieira157@gmail.com>
2025-06-06 18:16:00 -0300

    Removendo a utils.c impossibilitada de usar, e criando a função random pura.

--- src/assembly/program.asm
+++ src/assembly/program.asm
@@ -1,30 +1,53 @@
 section .data
-    alfabeto db 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 0
+    alfabeto db 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+    seed dq 0
+    newline db 10, 0
+    c dq 1013904223
+    a dq 1664525
+
+section .bss
+    letra_escolhida resb 1 ;
 
 section .text
-    global main
-    extern GenerateCode
-    extern printfRand
-    extern srand
-    extern time
+    global _start
 
-main:
-    call GenerateLoop
-    xor rax, rax
+_start:
+    call init_seed
+
+    call generate_code
+
+    mov rax, 60
+    mov rdi, 0
+    syscall
+
+
+; Função para gerar número pseudo-aleatório usando Linear Congruential Generator (LCG)
+; Fórmula: next = (a * seed + c) % m
+; Usando constantes como a=1664525, c=1013904223, m=2^32
+random_gen:
+    mov rax, [seed] 
+    mov rdx, [a]
+    mul rdx        ; rax = seed * a
+    add rax, [c]    ; rax + c
+    and rax, 0xFFFFFFFF     ; módulo mantém apenas 32 bits baixos
+    mov [seed], rax
     ret
 
-GenerateLoop:
-    ; Inicializar gerador de números aleatórios
-    xor rdi, rdi        ; time(NULL)
-    call time
-    mov rdi, rax        ; usar resultado de time() como seed
-    call srand
-    
-    ; Chamar GenerateCode
-    lea rdi, [alfabeto] ; passar endereço do alfabeto
-    call GenerateCode
-    
-    ; Chamar printfRand com o resultado
-    mov rdi, rax        ; resultado de GenerateCode está em rax
-    xor rax, rax
+; Função para obter seed baseado no tempo atual, mesma função do srand() do C
+init_seed: 
+    rdtsc       ; Lê contador de ciclos do processador
+    shl rdx, 32     ; contador de 64 bits
+    or rax, rdx     
+    mov [seed], rax
     ret
+
+generate_code:
+    call random_gen
+    xor rdx, rdx
+    mov rbx, 26
+    div rbx
+
+    mov al, [alfabeto + rdx]
+    mov [letra_escolhida], al
+    ret
+
