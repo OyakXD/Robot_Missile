177317ed610149a7b9fc872d69faf24a32da986b
ricardof72-jdk <ricardofilho702@gmail.com>
2025-06-08 19:30:00 -0300

    Adicionando função principal para a lógica do jogo

--- src/assembly/program.asm
+++ src/assembly/program.asm
@@ -5,20 +5,50 @@ section .data
     c dq 1013904223
     a dq 1664525
 
+    msg_start db 'ROBOT MISSILE', 10, 0
+    msg_input db 'TYPE THE CORRECT CODE: ', 0
+    msg_find db 'TICK..FZZZ...CLICK..!', 10, 0
+    msg_find2 db 'YOU DID IT', 10, 0
+    msg_after db 'LATER ', 0        
+    msg_before db 'EARLIER ', 0        
+    msg_output1 db 'LETTER (A-Z) TO', 10, 0
+    msg_output2 db 'DEFUSE THE MISSILE', 10, 0
+    msg_output3 db 'YOU HAVE 4 CHANCES', 10, 0
+    msg_invalid db 'ENTER ONLY LETTERS FROM A TO Z!', 10, 0
+    msg_game_over db 'YOU BLEW IT', 10, 0
+    msg_game_over2 db 'THE CORRET CODE WAS: ', 0
+    msg_game_over3 db 'BOOOOOOOOOOOOOOOOOM...', 10, 0
+    msg_chances db 'REMAINING CHANCES: ', 0
+
 section .bss
-    letra_escolhida resb 1 ;
+    codigo resb 1
+    tentativa resb 2
+    chances resb 1
 
 section .text
     global _start
 
 _start:
-    call init_seed
+    ; Inicializa o número de chances
+    mov byte [chances], 4
 
+    ; Função para gerar uma letra aleatória.
+    call init_seed
     call generate_code
 
-    mov rax, 60
-    mov rdi, 0
-    syscall
+    lea rdi, [msg_start]
+    call print_string
+    
+    lea rdi, [msg_output1]
+    call print_string
+    
+    lea rdi, [msg_output2]
+    call print_string
+    
+    lea rdi, [msg_output3]
+    call print_string
+    
+    jmp game_loop
 
 
 ; Função para gerar número pseudo-aleatório usando Linear Congruential Generator (LCG)
@@ -51,3 +81,70 @@ generate_code:
     mov [letra_escolhida], al
     ret
 
+game_loop:
+
+    ; Verifica se ainda tem chances
+    cmp byte [chances], 0
+    je game_over
+
+    lea rdi, [msg_chances]
+    call print_string
+    mov al, [chances]
+    add al, '0'             ; converte número para ASCII
+    call print_char
+    lea rdi, [newline]
+    call print_string
+
+    lea rdi, [msg_input]
+    call print_string
+
+input_loop:
+    call read_char
+    mov [tentativa], al
+
+    ; Validação: verifica se é uma letra maiúscula (A-Z)
+    cmp al, 'A'
+    jl invalid_input
+    cmp al, 'Z'
+    jg invalid_input
+
+
+    mov al, [tentativa]
+    mov bl, [codigo]
+    call missile
+
+    cmp al, 1
+    je game_won
+
+    dec byte [chances]
+    jmp game_loop
+
+invalid_input:
+    lea rdi, [msg_invalid]
+    call print_string
+    jmp input_loop
+
+game_won:
+    lea rdi, [msg_find]
+    call print_string
+    lea rdi, [msg_find2]
+    call print_string
+
+    mov rax, 60
+    mov rdi, 0
+    syscall
+
+game_over:
+    lea rdi, [msg_game_over3]
+    call print_string
+    lea rdi, [msg_game_over]
+    call print_string
+    lea rdi, [msg_game_over2]
+    call print_string
+    mov al, [codigo]        
+    call print_char
+    lea rdi, [newline]
+    call print_string
+    mov rax, 60
+    mov rdi, 1              
+    syscall
