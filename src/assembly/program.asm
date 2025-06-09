387e87492a128de5f288c2cafa6928cff1e5b2b7
ricardof72-jdk <ricardofilho702@gmail.com>
2025-06-09 20:49:14 -0300

    Usando registradores do assembly x86 para criar funções para gerar letras aleatórias

--- src/assembly/program.asm
+++ src/assembly/program.asm
@@ -1,9 +1,6 @@
 section .data
     alfabeto db 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
-    seed dq 0
     newline db 10, 0
-    c dq 1013904223
-    a dq 1664525
 
     msg_start db 'ROBOT MISSILE', 10, 0
     msg_input db 'TYPE THE CORRECT CODE: ', 0
@@ -33,7 +30,6 @@ _start:
     mov byte [chances], 4
 
     ; Função para gerar uma letra aleatória.
-    call init_seed
     call generate_code
 
     lea rdi, [msg_start]
@@ -50,27 +46,9 @@ _start:
     
     jmp game_loop
 
-
-; Função para gerar número pseudo-aleatório usando Linear Congruential Generator (LCG)
-; Fórmula: next = (a * seed + c) % m
-; Usando constantes como a=1664525, c=1013904223, m=2^32
 random_gen:
-    mov rax, [seed] 
-    mov rdx, [a]
-    mul rdx        ; rax = seed * a
-    add rax, [c]    ; rax + c
-    and rax, 0xFFFFFFFF     ; módulo mantém apenas 32 bits baixos
-    mov [seed], rax
+    rdrand rax
     ret
-
-; Função para obter seed baseado no tempo atual, mesma função do srand() do C
-init_seed: 
-    rdtsc       ; Lê contador de ciclos do processador
-    shl rdx, 32     ; contador de 64 bits
-    or rax, rdx     
-    mov [seed], rax
-    ret
-
 generate_code:
     call random_gen
     xor rdx, rdx
